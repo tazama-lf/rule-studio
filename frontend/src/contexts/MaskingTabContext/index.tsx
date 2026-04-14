363e9c8323fbf78978df800eca19a64499ca7622
Muhammad Ali <ali.shoaib@paysyslabs.com>
2026-04-14 14:06:04 +0500

    feat: refactor Tabs component to accept tabs and selectedTab as props, removing variant logic
    Signed-off-by: Muhammad Ali <ali.shoaib@paysyslabs.com>

--- frontend/src/contexts/MaskingTabContext/index.tsx
+++ frontend/src/contexts/MaskingTabContext/index.tsx
@@ -1,3 +1,1 @@
-export { MaskingTabContext, type MaskingTabContextType } from './MaskingTabContext'
 export { MaskingTabProvider } from './MaskingTabProvider'
-export { useMaskingTab } from './useMaskingTab'
